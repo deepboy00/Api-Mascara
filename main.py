7362f6d317564be75e8ab3ccba1ac941345fedac
deepboy00 <aquinoangelo2006@gmail.com>
2025-11-23 16:01:27 -0500

    arreglando el endpoint register_ip por sintaxis

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 from routes.Login.auth import auth_bp
 from routes.Login.register import register_bp
+from routes.Ip.register_ip import ip_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(register_bp)
+app.register_blueprint(ip_bp)
 
 @app.route('/')
 def index():
